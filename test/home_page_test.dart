f763269b690e33e1d094568fdca94232ce02d23b
Vikas Jain <jainv1354@gmail.com>
2025-07-17 00:38:34 +0530

    message

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,43 +1,32 @@
-import 'package:assessment/Features/Home/Data/repo_implementation.dart';
+import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:mockito/mockito.dart';
+import 'package:hive/hive.dart';
 
+import 'package:assessment/Features/Home/Data/repo_implementation.dart';
 
+void main() {
+  late Directory testDir;
 
-// Create a MockPathProvider
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    testDir = await Directory.systemTemp.createTemp();
+    Hive.init(testDir.path);
+    await Hive.openBox("dogBox");
+  });
 
+  tearDownAll(() async {
+    await Hive.deleteBoxFromDisk("dogBox");
+    Hive.close();
+  });
 
-void main() async {
-  // Ensure widgets binding is initialized for testing
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  // Mock the PathProvider behavior
-  final mockPathProvider = MockPathProvider();
-
-  // Assign the mock to the PathProviderPlatform instance
-  PathProviderPlatform.instance = mockPathProvider;
-
-  // Initialize Hive with a mocked directory
-  Hive.init('/mocked_directory'); // Do not use await here
-  await Hive.openBox("dogBox");
-
-  // Define the test
   test("fetch data repo when call function gives dog list modal", () async {
     // Arrange
-    final FetchDataImp fetchDataImp = FetchDataImp();
+    final fetchDataImp = FetchDataImp();
 
     // Act
-    var res = await fetchDataImp.fetchData();
+    final res = await fetchDataImp.fetchData();
 
     // Assert
     expect(res, isA<List<Map<String, dynamic>>>());
   });
-}
-
-class MockPathProvider extends Mock {
-  @override
-  Future<String> getApplicationDocumentsDirectory() async {
-    return '/mocked_directory'; // Mocked directory path
-  }
 }
